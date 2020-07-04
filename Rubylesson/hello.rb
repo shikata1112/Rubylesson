3bbf884cdf11a5c4ec9248a6bd72eacb1deb5fee
四方 秀 <shu@MacBook-Pro-2.local>
2020-07-04 14:01:06 +0900

    first commit

---
+++ Rubylesson/hello.rb
@@ -0,0 +1,49 @@
+# puts 'Hello, World'
+
+# puts 100 + "200".to_i
+
+# puts "WEBCAMP".length
+
+# puts "WEBCAMP".reverse
+
+# puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
+
+# 変数とは 5章
+
+# 6章
+
+# 7章　条件分岐
+
+# 8章　繰り返し処理
+# puts "キーボードから何か入力してみましょう"
+# input_key = gets
+# puts "入力された内容は" + input_key
+
+# puts "キーボードで数字「2」と数字「3」を入力してください"
+# a = gets.to_i
+# b = gets.to_i
+# puts "a+b=#{a+b}"
+
+# dice = 0
+# while dice != 6 do
+#   dice = rand(1..6)
+#   puts dice
+# end
+
+# for i in 1..10 do
+#   puts i
+# end 
+
+# {"apple"=>130, "strawberry"=>180, "orange"=>100}.each do |fruit, price|
+#   puts "#{fruit}は#{price}円です"
+# end
+
+# i = 0
+# while i <= 10 do
+#   if i > 5
+#     break
+#   end
+#   puts i
+#   i += 1
+# end
+
